db26dea30cb7151dee51b60a1a83cb115c1171f4
Rolukas <rolandosumoza@gmail.com>
2023-05-08 18:00:50 -0600

    change variables

--- src/database/db_connect.js
+++ src/database/db_connect.js
@@ -1,12 +1,12 @@
 const { Pool } = require("pg");
 
-const DATABASE_URL = process.env.POSTGRES_URL_NON_POOLING;
-
-const isProduction = process.env.ENVIRONMENT === "PRODUCTION";
+const DATABASE_URL =
+  process.env.POSTGRES_URL_NON_POOLING ||
+  'postgresql://postgres:""@localhost:5432/mycgi';
 
 const pool = new Pool({
   connectionString: DATABASE_URL,
-  ssl: isProduction ? true : { rejectUnauthorized: false },
+  ssl: process.env.POSTGRES_URL_NON_POOLING ? true : false,
 });
 
 module.exports = pool;
